3a5d2c9a1bb6118ae8b04be57cc852c5d6484035
Naveenkumar05-nk <jangidnaveen05@gmail.com>
2020-11-23 21:17:26 +0530

    Updates

---
+++ precache-manifest.4f5ca635efe316e9659311120c97eb06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7b39d2861c4d974cf0d70c1eac3eefc",
+    "url": "/PicStore/index.html"
+  },
+  {
+    "revision": "3497e061a83149119f82",
+    "url": "/PicStore/static/css/main.df4eb4f9.chunk.css"
+  },
+  {
+    "revision": "a33ea917ab10cd0341a2",
+    "url": "/PicStore/static/js/2.51ca6734.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/PicStore/static/js/2.51ca6734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3497e061a83149119f82",
+    "url": "/PicStore/static/js/main.a0288a8d.chunk.js"
+  },
+  {
+    "revision": "3ac02ef977137f748ffc",
+    "url": "/PicStore/static/js/runtime-main.22a2ea0e.js"
+  }
+]);
